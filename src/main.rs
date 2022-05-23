320e578f6b3846a6f5339efdcee6c2a1785bed97
adrianohellermylla <adrianohellermylla@gmail.com>
2022-05-22 22:31:43 -0300

    feat: Add generic user struct and methods

--- src/main.rs
+++ src/main.rs
@@ -20,22 +20,37 @@ fn main() -> Result<(),Box<dyn Error>>{
 
     let number_list: Vec<i32> = vec![120,1230,11,9,187];
 
-    let largest_number: i32 = get_largest(&number_list);
+    // Implemented via Prototype
+    let one: User<String,i32> = User::new(
+        String::from("Robles"),
+        String::from("robles@gmail.com"),
+        String::from("32cx4374b2849732b09432984v32042"),
+        124523682
+    );
 
-    println!("Largest number: {}",largest_number);
+    // Implemented via Generic Method
+    let new_user = create_new_user(String::from("Robles"),
+                    String::from("robles@gmail.com"),
+                    String::from("32cx4374b2849732b09432984v32042"),
+                    124523682);
+
+    println!("{:#?}",one);
+
+    println!("{:#?}",new_user);
 
     Ok(())
 }
 
-fn get_largest(item_list: &[i32]) -> i32 {
-    let mut largest_num = item_list[0];
-    for &item in item_list {
-        if item > largest_num {
-            largest_num = item;
-        }
-    }
-    largest_num
-}
+// fn get_largest<T>(item_list: &[T]) -> T {
+//     let mut largest = item_list[0];
+//     for &item in item_list {
+//         if item > largest {
+//             largest = item;
+//         }
+//     }
+//     largest
+// }
+
 // Error handling using Result -> Ok/Err
 fn read_all(ref_text: &str) -> Result<String,io::Error> {
     let mut string_info = String::new();
@@ -46,3 +61,41 @@ fn read_all(ref_text: &str) -> Result<String,io::Error> {
 fn last_char(ref_text: String) -> Option<char> {
     ref_text.lines().next()?.chars().last()
 }
+
+#[derive(Debug)]
+struct User<T,V> {
+    name: T,
+    email: T,
+    id: T,
+    created_at: V,
+}
+
+impl<T,V> User<T,V> {
+    fn new(name: T, email: T, id: T, created_at: V) -> User<T,V> {
+        User {
+            email,
+            name,
+            id,
+            created_at
+        }
+    }
+    fn name(&self) -> &T {
+        &self.name
+    }
+    fn email(&self) -> &T {
+        &self.email
+    }
+    fn id(&self) -> &T {
+        &self.id
+    }
+}
+
+fn create_new_user(name: String, email: String, id: String, created_at: i32) -> User<String,i32> {
+    User {
+        email,
+        name,
+        id,
+        created_at
+    }
+}
+
